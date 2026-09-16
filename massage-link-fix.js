fe7b61e545a13d99b3fd894a89de56e88750e529
innercodesystems <peter.wicklein@innercodesystems.com>
2026-09-16 23:37:47 +0200

    ICS Akademie Link auf neue HTML Seite umstellen

--- massage-link-fix.js
+++ massage-link-fix.js
@@ -1,8 +1,16 @@
-// ICS · Transformationsmassage Link
-// Die frühere Website-Route /massagebereich existiert nicht mehr.
+// ICS · Website Links
+// Frühere Website-Routen existieren nach der HTML-Umstellung nicht mehr.
 (() => {
   const massageLink = document.querySelector('a[href="https://innercodesystems.com/massagebereich"]');
-  if (!massageLink) return;
+  if (massageLink) {
+    massageLink.href = 'https://innercodesystems.github.io/ics-auswertungstool/transformationsmassage.html';
+  }
 
-  massageLink.href = 'https://innercodesystems.github.io/ics-auswertungstool/transformationsmassage.html';
+  const akademieLinks = document.querySelectorAll(
+    'a[href="https://innercodesystems.com/ics-akademie"], a[href="https://www.innercodesystems.com/ics-akademie"], a[href="https://n1594499.websitebuilder.online/ics-akademie"]'
+  );
+
+  akademieLinks.forEach((link) => {
+    link.href = 'https://innercodesystems.github.io/ics-auswertungstool/ics-akademie.html';
+  });
 })();
